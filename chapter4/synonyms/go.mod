3a5a3d698919302b51079feb0090f4a354ce127b
nnaka2992 <nnaka2992@gmail.com>
2021-07-10 14:06:08 +0900

    change file structure

--- chapter4/synonyms/go.mod
+++
@@ -1,5 +0,0 @@
-module chapter4/synonyms
-
-go 1.14
-
-require github.com/joho/godotenv v1.3.0
--- chapter4/thesaurus/synonyms/main.go
+++ chapter4/thesaurus/synonyms/main.go
@@ -4,23 +4,30 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"log"
 
 	"github.com/nnaka2992/go_web_app/chapter4/thesaurus"
 	"github.com/joho/godotenv"
 )
 
-// read env information
-var _ = godotenv.Load("test.env")
-
 func main() {
+	// read env information
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	envPath := filepath.Dir(ex) + "/test.env"
+	var _ = godotenv.Load(envPath)
 	apiKey := os.Getenv("BHT_APIKEY")
 	thesaurus := &thesaurus.BigHuge{APIKey: apiKey}
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
 		word := s.Text()
+		fmt.Println(word)
 		syns, err := thesaurus.Synonyms(word)
 		if err != nil {
+		fmt.Println(err)
 			log.Fatalf("Failed to query %q's synonyms: %v\n", word, err)
 		}
 		if len(syns) == 0 {
